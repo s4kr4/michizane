95e2312ac72dd208b86af5390f822815136577d0
s4kr4 <s4kr4m4@gmail.com>
2018-06-06 11:36:24 +0900

    Add Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import Header from '../components/Header'
 import Editor from '../containers/Editor'
 
 const App = () => (
   <div>
+    <Header />
     <Editor />
   </div>
 )
