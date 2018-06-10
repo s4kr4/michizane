6e3655e60a64e0796f07d2d2b33815b0cbc5ff97
s4kr4 <s4kr4m4@gmail.com>
2018-06-10 17:45:52 +0900

    Add markdown-it

--- src/modules/Editor/processor.js
+++ src/modules/Editor/processor.js
@@ -1,10 +1,30 @@
+import MarkdownIt from 'markdown-it'
+
 import { TranslateMode } from '.'
 
+const md = new MarkdownIt()
+
 export const translate = (input, translateMode) => {
+  const tree = md.parse(input)
+
   switch (translateMode) {
     case TranslateMode.QIITA_TO_GITHUB:
-      const output = input.replace('\n', '  \n')
-      return output
+      for (const token of tree) {
+        if (token.type === 'inline') {
+          token.content = token.content.replace(/\n/g, '  \n')
+        }
+
+        if (token.type === 'paragraph_close') {
+          token.content = '\n\n'
+        }
+      }
+
+      let output = ''
+      for (const token of tree) {
+        output += token.content
+      }
+
+      return output.replace(/(\s|\n)+$/g, '')
 
     default:
       return input
